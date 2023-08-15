bd15a1dcdb088ecf251e769affa3ce8e6806dcc4
Joanna K <joannakleinrm@gmail.com>
2023-08-15 16:35:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,4 @@
-// const app = require('./app')
-// const mongoose = require('mongoose');
-// mongoose.Promise = require('mongoose');
-// require('dotenv').config()
 
-// const uriDb = process.env.DB_HOST
-
-// mongoose.set("strictQuery", false);
-
-// const connection = mongoose.connect(uriDb, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// connection
-//   .then(() => {
-//     app.listen(3000, () => {
-//       console.log("Database connection successful");
-//     })
-//   })
-//   .catch((error) => {
-//     console.log(`Server error: ${error.message}`, uriDb);
-//     process.exit();
-//   }
-// )
 
 const express = require('express');
 const logger = require('morgan');
@@ -69,4 +45,33 @@ connection
 
 
 
+
+// _____ DO USUNIĘCIA _______
+
+
+// const app = require('./app')
+// const mongoose = require('mongoose');
+// mongoose.Promise = require('mongoose');
+// require('dotenv').config()
+
+// const uriDb = process.env.DB_HOST
+
+// mongoose.set("strictQuery", false);
+
+// const connection = mongoose.connect(uriDb, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// connection
+//   .then(() => {
+//     app.listen(3000, () => {
+//       console.log("Database connection successful");
+//     })
+//   })
+//   .catch((error) => {
+//     console.log(`Server error: ${error.message}`, uriDb);
+//     process.exit();
+//   }
+// )
 
