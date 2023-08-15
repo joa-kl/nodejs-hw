385b137228f15a3bb6b94958736adcb4f896f3d5
Joanna K <joannakleinrm@gmail.com>
2023-08-15 14:26:05 +0200

    up

--- server.js
+++ server.js
@@ -48,33 +48,14 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
 
-// app.use((_, res, __) => {
-//   res.status(404).json({
-//     status: 'error',
-//     code: 404,
-//     message: 'Use api on routes: /api/tasks',
-//     data: 'Not found',
-//   });
-// });
-
-// app.use((err, _, res, __) => {
-//   console.log(err.stack);
-//   res.status(500).json({
-//     status: 'fail',
-//     code: 500,
-//     message: err.message,
-//     data: 'Internal Server Error',
-//   });
-// });
-
-
 // const PORT = process.env.PORT || 3000;
 const uriDb = process.env.DB_HOST;
 
 const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
   // useFindAndModify: false,
-  useNewUrlParser: true,
+ 
 //   useUnifiedTopology: true,
 });
 
